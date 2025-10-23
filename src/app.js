83c3db4db2ed3266cf8248c46189b1d5275a6ac9
uvpatel <uvpatel7271@gmail.com>
2025-10-23 11:09:47 +0530

    do some changes

--- src/app.js
+++ src/app.js
@@ -4,8 +4,15 @@ import cors from "cors";
 
 const app = express();
 app.use(express.json());
+
+
+const allowedOrigins = [
+  "http://localhost:5173",     // local React
+  "https://auth-frontend-dtsr.vercel.app/"
+];
+
 app.use(cors({
-  origin: "http://localhost:5173",  // allow your React app
+  origin: allowedOrigins,  // allow your React app
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
